21ebd132cedfd9e965aaa4061cc8f16a1744f3bc
DeveloperLoser <jackwrath1@gmail.com>
2023-10-30 12:25:32 -0700

    aaa
    weather fully working

--- newsapi.py
+++ newsapi.py
@@ -1,9 +1,9 @@
-from newsapi import NewsApiClient
+import newsapi
 
-newsapi = NewsApiClient(api_key='232c2cbfd9f84018abcce502d1dc640e')
+news = newsapi.newsapi_client(api_key='232c2cbfd9f84018abcce502d1dc640e')
 
 def getSources():
-    sources = newsapi.get_sources()
+    sources = news.get_sources()
     savedsources = open("sources.txt", 'w')
     savedsources.writelines(sources)
 
---
+++ weather.py
@@ -0,0 +1,41 @@
+import requests
+from datetime import datetime
+import xml.etree.ElementTree as ET 
+
+def updateData():
+    weatherdata = requests.get("https://forecast.weather.gov/MapClick.php?lat=40.2109&lon=-75.3674&FcstType=digitalDWML")
+    newdata = open("newdata.xml", 'wb')
+    newdata.write(weatherdata.content)
+    newdata.close
+
+def parseData(data): # Get the root
+    tree = ET.parse(data)
+    root = tree.getroot()
+    return root
+
+def calcTime(): # Get the # of hours between now and tomorrow
+    now = datetime.now()
+    now = now.strftime('%H')
+    now = int(now)
+    return(23 - now)
+
+def gethHourlyTemp(root):
+    hourlyTemperature = []
+    for child in root.iter('temperature'):
+        if (child.attrib).get("type") == "hourly":
+            for val in child.findall("value"):
+                hourlyTemperature.append(val.text)
+    return hourlyTemperature
+
+def getRain(root):
+    rainchance = []
+    for child in root.iter('probability-of-precipitation'):
+        if (child.attrib).get("type") == "floating":
+            for val in child.findall("value"):
+                rainchance.append(val.text)
+    return rainchance
+
+if __name__ == "__main__":
+    updateData()
+    root = parseData('newdata.xml')
+    print(gethHourlyTemp(root)[0:calcTime()])
